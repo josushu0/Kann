4b7b6fd2424aa0a1319341f7170aac13edf97270
Josué Martell <josue.martell17@gmail.com>
2024-06-13 20:44:44 -0600

    refactor: welcome page route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Landing/Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
 });
 
