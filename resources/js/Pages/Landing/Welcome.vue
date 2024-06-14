ae6a84fd137b7cebd0115198af4f2fb58daf476d
Josué Martell <josue.martell17@gmail.com>
2024-06-13 20:20:37 -0600

    feat: configured shadcn

---
+++ resources/js/Pages/Landing/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+import Navbar from '@/Pages/Landing/Partials/Navbar.vue'
+
+defineProps({
+	canLogin: Boolean,
+	canRegister: Boolean,
+})
+</script>
+
+<template>
+	<div class="bg-background">
+		<Head title="Welcome" />
+		<Navbar :canLogin="canLogin" :canRegister="canRegister" />
+	</div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,10 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy'
+import { useColorMode } from '@vueuse/core'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+useColorMode()
 
 createInertiaApp({
 	title: (title) => `${title} - ${appName}`,
