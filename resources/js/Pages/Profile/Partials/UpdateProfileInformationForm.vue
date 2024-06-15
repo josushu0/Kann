82f72717fcf62d9d32b4d1271382f3c706971217
Josué Martell <josue.martell17@gmail.com>
2024-06-15 02:22:13 -0600

    feat: update profile information section in profile page

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref } from 'vue'
-import { Link, router, useForm } from '@inertiajs/vue3'
-import ActionMessage from '@/Components/ActionMessage.vue'
-import FormSection from '@/Components/FormSection.vue'
+import { router, useForm } from '@inertiajs/vue3'
+import { Label } from '@/Components/shadcn/ui/label/index.js'
+import { Input } from '@/Components/shadcn/ui/input/index.js'
+import { Card, CardContent } from '@/Components/shadcn/ui/card/index.js'
 import InputError from '@/Components/InputError.vue'
-import InputLabel from '@/Components/InputLabel.vue'
-import PrimaryButton from '@/Components/PrimaryButton.vue'
-import SecondaryButton from '@/Components/SecondaryButton.vue'
-import TextInput from '@/Components/TextInput.vue'
+import { Button } from '@/Components/shadcn/ui/button/index.js'
+import { Avatar, AvatarImage } from '@/Components/shadcn/ui/avatar/index.js'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps({
 	user: Object,
@@ -17,18 +17,11 @@ const form = useForm({
 	_method: 'PUT',
 	name: props.user.name,
 	email: props.user.email,
-	photo: null,
 })
 
-const verificationLinkSent = ref(null)
-const photoPreview = ref(null)
-const photoInput = ref(null)
+const verificationLinkSent = ref(false)
 
 const updateProfileInformation = () => {
-	if (photoInput.value) {
-		form.photo = photoInput.value.files[0]
-	}
-
 	form.post(route('user-profile-information.update'), {
 		errorBag: 'updateProfileInformation',
 		preserveScroll: true,
@@ -37,162 +30,79 @@ const updateProfileInformation = () => {
 }
 
 const sendEmailVerification = () => {
+	router.post(route('verification.send'))
 	verificationLinkSent.value = true
-}
-
-const selectNewPhoto = () => {
-	photoInput.value.click()
-}
-
-const updatePhotoPreview = () => {
-	const photo = photoInput.value.files[0]
-
-	if (!photo) return
-
-	const reader = new FileReader()
-
-	reader.onload = (e) => {
-		photoPreview.value = e.target.result
-	}
-
-	reader.readAsDataURL(photo)
-}
-
-const deletePhoto = () => {
-	router.delete(route('current-user-photo.destroy'), {
-		preserveScroll: true,
-		onSuccess: () => {
-			photoPreview.value = null
-			clearPhotoFileInput()
-		},
-	})
-}
-
-const clearPhotoFileInput = () => {
-	if (photoInput.value?.value) {
-		photoInput.value.value = null
-	}
 }
 </script>
 
 <template>
-	<FormSection @submitted="updateProfileInformation">
-		<template #title> Profile Information</template>
+	<form
+		@submit.prevent="updateProfileInformation"
+		class="grid gap-4 md:grid-cols-3">
+		<div>
+			<h2 class="font-bold">Profile information</h2>
+			<p class="text-sm">
+				Update your account's profile information and email address.
+			</p>
+		</div>
 
-		<template #description>
-			Update your account's profile information and email address.
-		</template>
-
-		<template #form>
-			<!-- Profile Photo -->
-			<div
-				v-if="$page.props.jetstream.managesProfilePhotos"
-				class="col-span-6 sm:col-span-4">
-				<!-- Profile Photo File Input -->
-				<input
-					id="photo"
-					ref="photoInput"
-					type="file"
-					class="hidden"
-					@change="updatePhotoPreview" />
-
-				<InputLabel for="photo" value="Photo" />
-
-				<!-- Current Profile Photo -->
-				<div v-show="!photoPreview" class="mt-2">
-					<img
-						:src="user.profile_photo_url"
-						:alt="user.name"
-						class="rounded-full h-20 w-20 object-cover" />
+		<Card class="col-span-2">
+			<CardContent class="mt-6 space-y-4">
+				<div class="flex flex-col items-center gap-6 sm:flex-row">
+					<Avatar class="size-40">
+						<AvatarImage :src="user.profile_photo_path" alt="Profile Image" />
+					</Avatar>
+					<Button class="flex items-center gap-2">
+						<Icon icon="tabler:brand-gravatar" class="size-4" />
+						<a href="https://gravatar.com" target="_blank">Edit Gravatar</a>
+					</Button>
 				</div>
-
-				<!-- New Profile Photo Preview -->
-				<div v-show="photoPreview" class="mt-2">
-					<span
-						class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
-						:style="'background-image: url(\'' + photoPreview + '\');'" />
+				<div>
+					<Label for="name">Name</Label>
+					<Input type="text" id="name" name="name" v-model="form.name" />
+					<InputError :message="form.errors.name" class="mt-2" />
 				</div>
-
-				<SecondaryButton
-					class="mt-2 me-2"
-					type="button"
-					@click.prevent="selectNewPhoto">
-					Select A New Photo
-				</SecondaryButton>
-
-				<SecondaryButton
-					v-if="user.profile_photo_path"
-					type="button"
-					class="mt-2"
-					@click.prevent="deletePhoto">
-					Remove Photo
-				</SecondaryButton>
-
-				<InputError :message="form.errors.photo" class="mt-2" />
-			</div>
-
-			<!-- Name -->
-			<div class="col-span-6 sm:col-span-4">
-				<InputLabel for="name" value="Name" />
-				<TextInput
-					id="name"
-					v-model="form.name"
-					type="text"
-					class="mt-1 block w-full"
-					required
-					autocomplete="name" />
-				<InputError :message="form.errors.name" class="mt-2" />
-			</div>
-
-			<!-- Email -->
-			<div class="col-span-6 sm:col-span-4">
-				<InputLabel for="email" value="Email" />
-				<TextInput
-					id="email"
-					v-model="form.email"
-					type="email"
-					class="mt-1 block w-full"
-					required
-					autocomplete="username" />
-				<InputError :message="form.errors.email" class="mt-2" />
-
+				<div>
+					<Label for="email">Email</Label>
+					<Input type="email" id="email" name="email" v-model="form.email" />
+					<InputError :message="form.errors.email" class="mt-2" />
+				</div>
 				<div
 					v-if="
 						$page.props.jetstream.hasEmailVerification &&
 						user.email_verified_at === null
 					">
-					<p class="text-sm mt-2">
+					<p
+						class="mt-2 flex flex-col items-start text-sm lg:flex-row lg:items-center">
 						Your email address is unverified.
 
-						<Link
-							:href="route('verification.send')"
-							method="post"
-							as="button"
-							class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-							@click.prevent="sendEmailVerification">
+						<Button
+							variant="link"
+							@click.prevent="sendEmailVerification"
+							class="text-wrap px-0 text-start underline lg:px-4">
 							Click here to re-send the verification email.
-						</Link>
+						</Button>
 					</p>
 
-					<div
-						v-show="verificationLinkSent"
-						class="mt-2 font-medium text-sm text-green-600">
+					<p v-show="verificationLinkSent" class="mt-2 text-sm font-medium">
 						A new verification link has been sent to your email address.
-					</div>
+					</p>
 				</div>
-			</div>
-		</template>
 
-		<template #actions>
-			<ActionMessage :on="form.recentlySuccessful" class="me-3">
-				Saved.
-			</ActionMessage>
-
-			<PrimaryButton
-				:class="{ 'opacity-25': form.processing }"
-				:disabled="form.processing">
-				Save
-			</PrimaryButton>
-		</template>
-	</FormSection>
+				<div class="flex items-center justify-end gap-2">
+					<transition
+						leave-active-class="transition ease-in duration-1000"
+						leave-from-class="opacity-100"
+						leave-to-class="opacity-0">
+						<span
+							v-show="form.recentlySuccessful"
+							class="text-sm text-gray-600">
+							Saved.
+						</span>
+					</transition>
+					<Button type="submit" :disabled="form.processing">Save</Button>
+				</div>
+			</CardContent>
+		</Card>
+	</form>
 </template>
--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -6,6 +6,7 @@ import SectionBorder from '@/Components/SectionBorder.vue'
 import TwoFactorAuthenticationForm from '@/Pages/Profile/Partials/TwoFactorAuthenticationForm.vue'
 import UpdatePasswordForm from '@/Pages/Profile/Partials/UpdatePasswordForm.vue'
 import UpdateProfileInformationForm from '@/Pages/Profile/Partials/UpdateProfileInformationForm.vue'
+import { Separator } from '@/Components/shadcn/ui/separator/index.js'
 
 defineProps({
 	confirmsTwoFactorAuthentication: Boolean,
@@ -15,42 +16,35 @@ defineProps({
 
 <template>
 	<AppLayout title="Profile">
-		<template #header>
-			<h2 class="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>
-		</template>
+		<div class="container">
+			<div v-if="$page.props.jetstream.canUpdateProfileInformation">
+				<UpdateProfileInformationForm :user="$page.props.auth.user" />
+				<Separator class="my-6" />
+			</div>
 
-		<div>
-			<div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-				<div v-if="$page.props.jetstream.canUpdateProfileInformation">
-					<UpdateProfileInformationForm :user="$page.props.auth.user" />
+			<div v-if="$page.props.jetstream.canUpdatePassword">
+				<UpdatePasswordForm class="mt-10 sm:mt-0" />
 
-					<SectionBorder />
-				</div>
+				<SectionBorder />
+			</div>
 
-				<div v-if="$page.props.jetstream.canUpdatePassword">
-					<UpdatePasswordForm class="mt-10 sm:mt-0" />
-
-					<SectionBorder />
-				</div>
-
-				<div v-if="$page.props.jetstream.canManageTwoFactorAuthentication">
-					<TwoFactorAuthenticationForm
-						:requires-confirmation="confirmsTwoFactorAuthentication"
-						class="mt-10 sm:mt-0" />
-
-					<SectionBorder />
-				</div>
-
-				<LogoutOtherBrowserSessionsForm
-					:sessions="sessions"
+			<div v-if="$page.props.jetstream.canManageTwoFactorAuthentication">
+				<TwoFactorAuthenticationForm
+					:requires-confirmation="confirmsTwoFactorAuthentication"
 					class="mt-10 sm:mt-0" />
 
-				<template v-if="$page.props.jetstream.hasAccountDeletionFeatures">
-					<SectionBorder />
-
-					<DeleteUserForm class="mt-10 sm:mt-0" />
-				</template>
+				<SectionBorder />
 			</div>
+
+			<LogoutOtherBrowserSessionsForm
+				:sessions="sessions"
+				class="mt-10 sm:mt-0" />
+
+			<template v-if="$page.props.jetstream.hasAccountDeletionFeatures">
+				<SectionBorder />
+
+				<DeleteUserForm class="mt-10 sm:mt-0" />
+			</template>
 		</div>
 	</AppLayout>
 </template>
