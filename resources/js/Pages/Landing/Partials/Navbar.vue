0ff331d233382ffe3aa105b29d52c2be1e57fd4f
Josué Martell <josue.martell17@gmail.com>
2024-06-15 00:56:21 -0600

    feat: added theme selector to landing page

--- resources/js/Pages/Landing/Partials/Navbar.vue
+++ resources/js/Pages/Landing/Partials/Navbar.vue
@@ -8,6 +8,7 @@ import {
 	SheetTrigger,
 } from '@/Components/shadcn/ui/sheet/index.js'
 import { Icon } from '@iconify/vue'
+import ThemeSelector from '@/Components/ThemeSelector.vue'
 
 defineProps({
 	canLogin: Boolean,
@@ -24,54 +25,57 @@ defineProps({
 				<span class="sr-only">Home</span>
 				<ApplicationLogo class="size-9 fill-background stroke-primary" />
 			</Link>
-			<div class="hidden items-center gap-2 md:flex" v-if="canLogin">
-				<Button as-child v-if="$page.props.auth.user">
-					<Link :href="route('projects.show')">Dashboard</Link>
-				</Button>
-				<template v-else>
-					<Button as-child>
-						<Link :href="route('login')">Login</Link>
+			<div class="flex items-center gap-2">
+				<div class="hidden items-center gap-2 md:flex" v-if="canLogin">
+					<Button as-child v-if="$page.props.auth.user">
+						<Link :href="route('projects.show')">Dashboard</Link>
 					</Button>
-					<Button as-child variant="outline" v-if="canRegister">
-						<Link :href="route('register')">Register</Link>
-					</Button>
-				</template>
-			</div>
-			<Sheet>
-				<SheetTrigger as-child class="md:hidden">
-					<Button variant="outline" size="icon">
-						<Icon icon="lucide:menu" class="size-4" />
-					</Button>
-				</SheetTrigger>
-				<SheetContent side="right">
-					<div class="mt-10 space-y-2" v-if="canLogin">
-						<Button
-							v-if="$page.props.auth.user"
-							as-child
-							variant="ghost"
-							size="lg"
-							class="w-full justify-start text-lg font-bold">
-							<Link :href="route('projects.show')">Dashboard</Link>
+					<template v-else>
+						<Button as-child>
+							<Link :href="route('login')">Login</Link>
 						</Button>
-						<template v-else>
+						<Button as-child variant="outline" v-if="canRegister">
+							<Link :href="route('register')">Register</Link>
+						</Button>
+					</template>
+				</div>
+				<ThemeSelector />
+				<Sheet>
+					<SheetTrigger as-child class="md:hidden">
+						<Button variant="outline" size="icon">
+							<Icon icon="lucide:menu" class="size-4" />
+						</Button>
+					</SheetTrigger>
+					<SheetContent side="right">
+						<div class="mt-10 space-y-2" v-if="canLogin">
 							<Button
+								v-if="$page.props.auth.user"
 								as-child
 								variant="ghost"
 								size="lg"
 								class="w-full justify-start text-lg font-bold">
-								<Link :href="route('login')">Login</Link>
+								<Link :href="route('projects.show')">Dashboard</Link>
 							</Button>
-							<Button
-								as-child
-								variant="ghost"
-								size="lg"
-								class="w-full justify-start text-lg font-bold">
-								<Link :href="route('register')">Register</Link>
-							</Button>
-						</template>
-					</div>
-				</SheetContent>
-			</Sheet>
+							<template v-else>
+								<Button
+									as-child
+									variant="ghost"
+									size="lg"
+									class="w-full justify-start text-lg font-bold">
+									<Link :href="route('login')">Login</Link>
+								</Button>
+								<Button
+									as-child
+									variant="ghost"
+									size="lg"
+									class="w-full justify-start text-lg font-bold">
+									<Link :href="route('register')">Register</Link>
+								</Button>
+							</template>
+						</div>
+					</SheetContent>
+				</Sheet>
+			</div>
 		</nav>
 	</header>
 </template>
