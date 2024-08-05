4ead3763e60b9c0ee934fc9ecd4384857d0c630b
Josué Martell <josue.martell17@gmail.com>
2024-08-05 13:09:28 -0600

    feat: added user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $sort = $request->query('sortColumn', 'id');
+        $order = $request->query('sortOrder', 'asc');
+        $rows = $request->query('rowCount', 20);
+        $users = User::orderBy($sort, $order)->paginate($rows);
+
+        return Inertia::render('Users/List', [
+            'users' => $users,
+            'sort' => [
+                'id' => $sort,
+                'order' => $order,
+            ],
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Users/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, CreateNewUser $action)
+    {
+        $action->create($request->all());
+
+        return to_route('users.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return Inertia::render('Users/Edit', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user, UpdateUserProfileInformation $action)
+    {
+        $action->update($user, $request->all());
+
+        return to_route('users.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return back();
+    }
+}
