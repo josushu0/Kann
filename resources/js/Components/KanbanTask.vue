5ced116ad3c10e8d06b437f5cecb04da3f5d0a7d
Josué Martell <josue.martell17@gmail.com>
2024-08-05 16:15:50 -0600

    feat: make task title not clip outside column

--- resources/js/Components/KanbanTask.vue
+++ resources/js/Components/KanbanTask.vue
@@ -12,16 +12,12 @@ const props = defineProps({
 </script>
 
 <template>
-	<CreateTask
-		:task="task"
-		:column="column"
-		:columns="columns"
-		:teamMembers="teamMembers">
-		<Button variant="ghost" class="flex w-full select-none justify-start p-2">
+	<CreateTask :task="task" :column="column" :columns="columns" :teamMembers="teamMembers">
+		<Button variant="ghost" class="flex w-full select-none justify-start p-2 h-fit">
 			<div class="task_drag_handle flex-none cursor-grab p-3">
 				<Icon icon="lucide:grip-vertical" />
 			</div>
-			{{ task.name }}
+			<span class="text-wrap text-start break-words max-w-[182px]">{{ task.name }}</span>
 		</Button>
 	</CreateTask>
 </template>
