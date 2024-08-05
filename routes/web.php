7864f22a82fc51082220f317c213b56b03cde2a9
Josué Martell <josue.martell17@gmail.com>
2024-08-05 13:05:41 -0600

    feat: added users route

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use App\Http\Controllers\ColumnController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/projects');
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::resource('users', UserController::class)->except('show');
     Route::resource('projects', ProjectController::class);
 
     Route::resource('columns', ColumnController::class)->only('store', 'update', 'destroy');
